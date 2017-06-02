9e458e3983e622857ae3da20cc43dfb75b7018c3
madhusudan1234 <madhusudan.vratesh007@gmail.com>
2017-06-02 23:59:54 +0530

    added seg_or_exp screen

--- app/src/main/java/com/example/mandeep/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mandeep/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mandeep.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    TextView continue_without_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continue_without_login = (TextView) findViewById(R.id.continue_without_login);
+
+        continue_without_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SegOrExp.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mandeep/myapplication/SegOrExp.java
@@ -0,0 +1,17 @@
+package com.example.mandeep.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by MANDEEP on 6/2/2017.
+ */
+
+public class SegOrExp extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seg_or_exp);
+    }
+}
